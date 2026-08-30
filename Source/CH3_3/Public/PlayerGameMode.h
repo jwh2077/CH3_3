600525d02ef9aa31f6d3727096c8975264ca522c
jwh2077 <jwh2077@gmail.com>
2026-08-30 12:11:48 +0900

    Initial commit

---
+++ Source/CH3_3/Public/PlayerGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PlayerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH3_3_API APlayerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CH3_3/Public/Player_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Player_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH3_3_API APlayer_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
