31a6333aa9a67bfce81c363778092ec48299c962
jwh2077 <jwh2077@gmail.com>
2026-08-31 19:44:14 +0900

    Add item system base classes and spawn volume
    Introduce core item system skeleton: add IItemInterface, ItemBase (implements interface, collision setup, overlap handlers, ActivateItem/DestroyItem, PickupEffect/PickupSound, ItemType), CoinItem subclass, ItemSpawnRow, and ASpawnVolum actor with basic lifecycle. Add corresponding .cpp implementations and small .idea project files. Prepares foundation for item spawning and pickup mechanics.

---
+++ Source/CH3_3/Public/CoinItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "CoinItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH3_3_API ACoinItem : public AItemBase
+{
+	GENERATED_BODY()
+	
+};
--- Source/CH3_3/Public/ItemBase.h
+++ Source/CH3_3/Public/ItemBase.h
@@ -4,12 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ItemInterface.h"
 #include "ItemBase.generated.h"
 
 class USphereComponent;
 
+
 UCLASS()
-class CH3_3_API AItemBase : public AActor
+class CH3_3_API AItemBase : public AActor, public IItemInterface
 {
 	GENERATED_BODY()
 	
@@ -17,34 +19,58 @@ public:
 
 	AItemBase();
 
-	// 씬 컴포넌트
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+protected:
+
+	//씬 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
 	USceneComponent* SceneComponent;
 	//스태틱 메시 컴포넌트
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
 	UStaticMeshComponent* StaticMeshComponent;
 	//구	형 콜리전 컴포넌트
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	USphereComponent* SphereCollisionComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USphereComponent* CollisionComponent;
+	//아이템 획득 이펙트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	UParticleSystem* PickupEffect;
+	//아이템 획득 사운드
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	USoundBase* PickupSound;
+
 
 	//아이템 오버랩 이벤트 함수
 	virtual void OnItemOverlap(
-		UPrimitiveComponent* OverlappedComponent,
+		UPrimitiveComponent* OverlappedComp,
 		AActor* OtherActor,
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex,
 		bool bFromSweep,
-		const FHitResult& SweepResult);
+		const FHitResult& SweepResult) override;
+
 	//아이템 오버랩 종료 이벤트 함수
 	virtual void OnItemEndOverlap(
-		UPrimitiveComponent* OverlappedComponent,
+		UPrimitiveComponent* OverlappedComp,
 		AActor* OtherActor,
 		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex);
+		int32 OtherBodyIndex) override;
 
-protected:
+	//아이템 작동 함수
+	virtual void ActivateItem(AActor* OtherActor);
+
+	//아이템 삭제 함수
+	virtual void DestroyItem();
+
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	//아이템 타입 Get 함수
+	virtual FName GetItemType() const override;
+
+
+	//아이템 타입
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemType;
 
 public:	
 	// Called every frame
